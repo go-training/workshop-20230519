937e4564f4915a7e6a640a92909837c864323d5c
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-05-19 15:34:21 +0800

    docs: improve code clarity and update dependencies
    - Add comments to explain the behavior of the `foo()` and `bar()` functions in the array-slice example
    - Create a new `go.mod` file for the struct make-vs-new example with Go version 1.20
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- 07-array-slice/example02/main.go
+++ 07-array-slice/example02/main.go
@@ -9,24 +9,24 @@ func main() {
 
 func foo() {
 	a := [3]int{1, 2, 3}
-	for k, v := range a {
+	for k, v := range a { // copy
 		if k == 0 {
 			a[0], a[1] = 100, 200
-			fmt.Println(a)
+			fmt.Println(a) // [100, 200, 3]
 		}
-		a[k] = 100 + v
+		a[k] = 100 + v // a[0] = 100 + 1, a[1] = 100 + 2, a[2] = 100 + 3
 	}
-	fmt.Println(a)
+	fmt.Println(a) // [101, 102, 103]
 }
 
 func bar() {
 	a := []int{1, 2, 3}
-	for k, v := range a {
+	for k, v := range a { // reference
 		if k == 0 {
 			a[0], a[1] = 100, 200
-			fmt.Println(a)
+			fmt.Println(a) // [100, 200, 3]
 		}
-		a[k] = 100 + v
+		a[k] = 100 + v // a[0] = 100 + 1, a[1] = 100 + 200, a[2] = 100 + 3
 	}
-	fmt.Println(a)
+	fmt.Println(a) // [101, 300, 103]
 }
